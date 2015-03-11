c1067250109a24eda536ae3e78dd1db9106d8b56
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-11 15:45:31 +0000

    Test drove retrieving items from the order

--- spec/orderSpec.js
+++ spec/orderSpec.js
@@ -3,11 +3,23 @@ var chai 		= require('chai');
 var expect 	= chai.expect;
 
 describe('Order', function() {
-	it('should be able to place an order', function(done) { 
-		var order = new Order();
+
+	var order;
+
+	beforeEach(function() { 
+		order = new Order();
+	});
+
+	it('should be able to add to an order', function(done) { 
 		order.addToItems('Cafe Latte', function(totalOrder) { 
 			expect(totalOrder).to.contain({"Cafe Latte": 4.75});
 			done();
 		});
 	});
+
+	it('should be able to retrieve items from an order', function() { 
+		expect(order.items()).to.contain({});
+	});
+
+	
 });
