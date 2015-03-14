016e807291dc127a112c35869d5824c57bda3b17
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-14 17:42:07 +0000

    Created test to check items with price are being added to the receipt

--- test/features/receiptFeature.js
+++ test/features/receiptFeature.js
@@ -3,16 +3,12 @@ var PDFtotextjs  		= require('pdftotextjs');
 var expect					= require('chai').expect;
 var fs 							= require('fs');
 var Receipt 				= require('../../src/receipt');
+var Order 					= require('../../src/order');
 
 describe('Receipt', function() { 
 
 	var pdf; 
 	var data;
-
-	beforeEach(function() { 
-		var receipt = new Receipt();
-		receipt.create('test');
-	});
 
 	afterEach(function() {
 		var filePath = "test.pdf";
@@ -24,11 +20,25 @@ describe('Receipt', function() {
 		data = pdf.getTextSync();
 	}
 
-	it('testing setup', function(done) { 
+	function createReceipt(order) { 
+		var receipt = new Receipt();
+		receipt.create('test', order);
+	}
+
+
+	function createOrder() { 
+		var order = new Order();
+		order.addToItems('Cafe Latte', function() { 
+			createReceipt(order.items());
+		});
+	};
+
+	it('should be able to display ordered items with a price', function(done) { 
+		createOrder();
 		this.timeout(2000);
 		setTimeout(function() {
  			readPDF();
- 			expect(data).to.contain('hey there');
+ 			expect(data).to.contain('Cafe Latte: 3.75');
  			done();
 		}, 1500);
 	});
