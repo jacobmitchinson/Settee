b496bfc2f31ec1df817733f775be553d7a51e40e
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-15 13:03:14 +0000

    Receipt now able to calculate total and add tax

--- src/receipt.js
+++ src/receipt.js
@@ -1,5 +1,6 @@
-var PDFDocument = require('pdfkit');
-var fs = require('fs');
+var PDFDocument     = require('pdfkit');
+var fs              = require('fs');
+var CalculateTotal  = require('./calculateTotal');
 
 var Receipt = function() { 
 	this.doc = new PDFDocument();
@@ -8,6 +9,8 @@ var Receipt = function() {
 Receipt.prototype.create = function(name, order) {
   this.doc.pipe(fs.createWriteStream(name + '.pdf'));
   this._addAllItems(order);
+  this._addTotal(order);
+  this._addTotalWithTax(order);
   this.doc.end();
 };
 
@@ -15,12 +18,26 @@ Receipt.prototype._addText = function(text) {
 	this.doc.text(text);
 };
 
-Receipt.prototype._addAllItems = function(order) {
+Receipt.prototype._addAllItems = function(order) {  
 	var itemsWithPrice = this._itemsWithPrice(order);
   var length = itemsWithPrice.length;
   for(var i = 0; i < length; i++) { 
   	this._addText(itemsWithPrice[i][0]);
   };
+};
+
+Receipt.prototype._addTotal = function(order) {
+  var calculateTotal = new CalculateTotal();
+  var total = calculateTotal.total(order);
+  var totalText = 'Cost: ' + '£' + total; 
+  this._addText(totalText);
+};
+
+Receipt.prototype._addTotalWithTax = function(order) {
+  var calculateTotal = new CalculateTotal();
+  var totalWithTax = calculateTotal.totalWithTax(order);
+  var totalWithTaxText = 'Total: ' + '£' + totalWithTax;
+  this._addText(totalWithTaxText);
 };
 
 // TODO: not sure this belongs here
@@ -30,7 +47,7 @@ Receipt.prototype._itemsWithPrice = function(order) {
 	var length = items.length;
 	var pairs = Array(length);
 	for (var i = 0; i < length; i++) {
-	  pairs[i] = [items[i] + ': ' + order[items[i]]];
+	  pairs[i] = [items[i] + ': ' + '£' + (order[items[i]]).toFixed(2)];
 	}
 	return pairs;
 };
