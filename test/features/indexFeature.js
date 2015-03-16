d519e66a3d35348ca88465f2d23fed4ef10f5bea
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-16 13:33:59 +0000

    Added test to display total for the order

--- test/features/indexFeature.js
+++ test/features/indexFeature.js
@@ -5,30 +5,34 @@ describe('Homepage', function() {
 
 	var client = {};
 
-  before(function(done) {
+  beforeEach(function(done) {
     client = webdriverio.remote({ desiredCapabilities: { browserName: 'chrome'}});
     client.init(done);
   });
 
-  after(function(done) {
+  afterEach(function(done) {
     client.end(done);
   });
 
-	it('should enable the user to enter the order', function(done) { 
-		client
+  function enterOrder() {
+  	client
 			.url('http://localhost:3000')
-			.getValue('#select',function(err,val){
-      	console.log(val); 
-    	})
 			.click('//*[@id="item-dropdown"]/option[1]')
 			.click('#submit')
+			.url('http://localhost:3000')
+			.click('//*[@id="item-dropdown"]/option[2]')
+			.click('#submit')
+		return client;
+  };
+
+	it('should enable the user to enter the order', function(done) { 
+		enterOrder()
 			.getText('#current-items', function(err, text) { 
 				expect(text).to.contain('Cafe Latte: £4.75')
 			})
 			.call(done);
 	});
 
-	
   it('should enable the user to reset the order', function(done) { 
   	client
   		.url('http://localhost:3000')
@@ -36,6 +40,14 @@ describe('Homepage', function() {
   		.url('http://localhost:3000')
   		.getText('#current-orders', function(err, text) { 
   			expect(text).not.to.contain('Cafe Latte: £4.75')
+  		})
+  		.call(done);
+  });
+
+  it('should be able to display the total for the order', function(done) { 
+  	enterOrder()
+  		.getText('#display-total', function(err, text) { 
+  			expect(text).to.contain('Total: £9.50')
   		})
   		.call(done);
   });
