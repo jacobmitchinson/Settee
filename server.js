eea319095da5c21d0e86d661bb09ee82fbdfff40
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-03-17 14:11:36 +0000

    Updated server.js to pass test and send receipt

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ var bodyParser = require('body-parser');
 var Order = require('./src/order');
 var Menu = require('./src/menu');
 var CalculateTotal = require('./src/calculateTotal');
+var Receipt = require('./receipt');
+var	SendReceipt = require('./src/sendReceipt'); 
 
 var menu = new Menu('hipstercoffee.json');
 var order = new Order();
 var calculateTotal = new CalculateTotal();
+var receipt = new Receipt();
+var sendReceipt = new SendReceipt();
+
+var uuid = require('node-uuid');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,6 +27,18 @@ function menuItems(callback) {
 function resetOrder() { 
 	order = new Order();
 };
+
+function sendReceiptHelper(receiptName) { 
+	sendReceipt.to('jmitchinson@gmail.com');
+	sendReceipt.from('jmitchinson@gmail.com');
+	sendReceipt.subject('Your Receipt');
+	sendReceipt.receipt(receiptName);
+	sendReceipt.message('Thanks for stopping by at the Coffee Connection. Here\'s your receipt.');
+	sendReceipt.send(function(err, hasSent) {
+		expect(hasSent).to.eql({message: 'success'});
+		done();
+	});
+}
 
 app.set('view engine', 'ejs');
 
@@ -39,6 +57,12 @@ app.post('/order_item', function(req, res) {
 app.post('/reset_order', function(req, res) { 
 	resetOrder();
 	res.redirect('/');
+});
+
+app.post('/send_receipt', function(req, res) { 
+	var receiptNumber = uuid.v1();
+	receipt(receiptNumber, order);
+	sendReceiptHelper(req.body.email);
 });
 
 module.exports = app;
